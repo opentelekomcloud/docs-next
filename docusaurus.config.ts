bbc32ce052bbdc5d89947ed916d7cd1df723701b
Kyriakos Akriotis <akriotis.kyriakos@gmail.com>
2024-07-08 19:35:07 +0200

    added process.env in config.ts

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -224,12 +224,12 @@ const config: Config = {
       typesenseServerConfig: {
         nodes: [
           {
-            host: "docs-search.hypelens.de",
-            port: 80,
-            protocol: "http",
+            host: process.env.TYPESENSE_HOST,
+            port: process.env.TYPESENSE_PORT,
+            protocol: process.env.TYPESENSE_PROTOCOL,
           },
         ],
-        apiKey: "j2Q51UsLuh9guYtc4x147Mv5X74g4iSI",
+        apiKey: process.env.TYPESENSE_API_KEY,
       },
 
       // Optional: Typesense search parameters: https://typesense.org/docs/0.24.0/api/search.html#search-parameters
