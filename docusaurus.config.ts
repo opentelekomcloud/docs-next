929381176ff65675b854522eaac133d4580c5076
Kyriakos Akriotis <kyriakos.akriotis@outlook.com>
2024-07-12 00:48:55 +0200

    test gitops wf e2e

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -3,7 +3,7 @@ import type { Config } from '@docusaurus/types';
 import type * as Preset from '@docusaurus/preset-classic';
 
 const config: Config = {
-  title: 'Architecture Center',
+  title: 'Architecture Center - GitOps',
   tagline: 'Best Practices & Blueprints',
   favicon: 'img/favicon.ico',
 
