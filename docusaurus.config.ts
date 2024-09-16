fb1fae3dabb78c99960b055fcdc6ebd5a7d9de81
Kyriakos Akriotis <akriotis.kyriakos@gmail.com>
2024-09-17 01:31:06 +0200

    removed trailing slashes, null coaleshing for baseUrl

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -12,13 +12,12 @@ const config: Config = {
   url: 'https://' + process.env.REACT_APP_DOCS_NEXT_HOST,
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/', //process.env.REACT_APP_DOCUSAURUS_BASE_URL,
+  baseUrl: process.env.REACT_APP_DOCUSAURUS_BASE_URL ?? '/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
   organizationName: process.env.REACT_APP_DOCS_NEXT_ORG, // Usually your GitHub org/user name.
   projectName: 'docs-next', // Usually your repo name.
-  trailingSlash: false,
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
