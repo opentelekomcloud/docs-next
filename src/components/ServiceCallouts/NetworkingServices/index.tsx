b93279353f41c9ba1e64ff76b3e59d2d2f28a39e
Sebastian Gode <sebastian.gode@telekom.de>
2024-11-06 11:33:21 +0000

    Fix NetworkingServices

--- src/components/ServiceCallouts/NetworkingServices/index.tsx
+++ src/components/ServiceCallouts/NetworkingServices/index.tsx
@@ -1,74 +1,83 @@
-import clsx from 'clsx';
-import Heading from '@theme/Heading';
-import styles from './styles.module.css';
 import ServiceCallouts, { Callout } from './../callout';
+
+import DcSvg from '@site/static/img/services/DC_DirectConnect.svg'
+import DnsSvg from '@site/static/img/services/DNS.svg'
+import EipSvg from '@site/static/img/services/IP-EIP.svg'
+import ElbSvg from '@site/static/img/services/ELB.svg'
+import ErSvg from '@site/static/img/services/er.svg'
+import NatgwSvg from '@site/static/img/services/nat-nat-gateway.svg'
+import PlasSvg from '@site/static/img/services/plas.svg'
+import SmgSvg from '@site/static/img/services/smg.svg'
+import VpcSvg from '@site/static/img/services/VPC.svg'
+import VpcepSvg from '@site/static/img/services/Vpc Endpoint.svg'
+import VpnSvg from '@site/static/img/services/VPN.svg'
 
  const calloutsList: Callout[] = [
     {
       title: "Direct Connect",
       text: "Dedicated Network Connection",
       link: "/docs/tags/direct-connect",
-      icon: require('@site/static/img/services/DC_DirectConnect.svg').default,
+      icon: DcSvg
     },
     {
         title: "DNS",
         text: "Domain Name Service",
         link: "/docs/tags/dns",
-        icon: require('@site/static/img/services/DNS.svg').default,
+        icon: DnsSvg
     },
     {
       title: "EIP",
       text: "Elastic IP",
       link: "/docs/tags/eip",
-      icon: require('@site/static/img/services/IP-EIP.svg').default,
+      icon: EipSvg
     },
     {
       title: "ELB",
       text: "Elastic Load Balancing",
       link: "/docs/tags/elb",
-      icon: require('@site/static/img/services/ELB.svg').default,
+      icon: ElbSvg
     },
     {
       title: "Enterprise Router",
       text: "Cloud Router Service",
       link: "/docs/tags/enterprise-router",
-      icon: require('@site/static/img/services/er.svg').default,
+      icon: ErSvg
     },
     {
       title: "NATGW",
       text: "NAT Gateway",
       link: "/docs/tags/natgw",
-      icon: require('@site/static/img/services/nat-nat-gateway.svg').default,
+      icon: NatgwSvg
     },
     {
       title: "PLAS",
       text: "Private Link Access Service",
       link: "/docs/tags/plas",
-      icon: require('@site/static/img/services/plas.svg').default,
+      icon: PlasSvg
     },
     {
       title: "Secure Mail Gateway",
       text: "Anti-Spam & Anti-Junk Outgoing Email Traffic",
       link: "/docs/tags/smg",
-      icon: require('@site/static/img/services/smg.svg').default,
+      icon: SmgSvg
     },
     {
       title: "VPC",
       text: "Virtual Private Cloud",
       link: "/docs/tags/vpc",
-      icon: require('@site/static/img/services/VPC.svg').default,
+      icon: VpcSvg
     },
     {
       title: "VPCEP",
       text: "Virtual Private Cloud Endpoint",
       link: "/docs/tags/vpcep",
-      icon: require('@site/static/img/services/Vpc Endpoint.svg').default,
+      icon: VpcepSvg
     },
     {
       title: "VPN",
       text: "Virtual Private Network",
       link: "/docs/tags/vpn",
-      icon: require('@site/static/img/services/VPN.svg').default,
+      icon: VpnSvg
     },
   ];
 
