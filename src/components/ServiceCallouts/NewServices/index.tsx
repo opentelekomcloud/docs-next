6eede4d769d4e6d7c96ac9e9828cf6d5e0605da9
Sebastian Gode <sebastian.gode@telekom.de>
2024-11-06 11:37:48 +0000

    Fix newServices

--- src/components/ServiceCallouts/NewServices/index.tsx
+++ src/components/ServiceCallouts/NewServices/index.tsx
@@ -1,44 +1,48 @@
-import clsx from 'clsx';
-import Heading from '@theme/Heading';
-import styles from './styles.module.css';
 import ServiceCallouts, { Callout } from '../callout';
+
+import AsmSvg from '@site/static/img/services/asm.svg'
+import CciSvg from '@site/static/img/services/cci.svg'
+import ConfigSvg from '@site/static/img/services/rms.svg'
+import OcrSvg from '@site/static/img/services/ocr.svg'
+import FgSvg from '@site/static/img/services/fg.svg'
+import GeminidbSvg from '@site/static/img/services/RDS .svg'
 
  const calloutsList: Callout[] = [
     {
       title: "ASM",
       text: "Application Service Mesh (Istio-Based)",
       link: "https://docs.otc.t-systems.com/application-service-mesh/index.html",
-      icon: require('@site/static/img/services/asm.svg').default,
+      icon: AsmSvg
     },
     {
       title: "CCI",
       text: "Serverless Container Engine",
       link: "https://docs.otc.t-systems.com/cloud-container-instance/index.html",
-      icon: require('@site/static/img/services/cci.svg').default,
+      icon: CciSvg
     },
     {
       title: "Config",
       text: "Continuously Evaluate Resource Configuration",
       link: "https://docs.otc.t-systems.com/config/index.html",
-      icon: require('@site/static/img/services/rms.svg').default,
+      icon: ConfigSvg
     },
     {
       title: "OCR",
       text: "Optical Character Recognition",
       link: "https://docs.otc.t-systems.com/optical-character-recognition/index.html",
-      icon: require('@site/static/img/services/ocr.svg').default,
+      icon: OcrSvg
     },
     {
       title: "FGS",
       text: "FunctionGraph",
       link: "https://docs.otc.t-systems.com/function-graph/index.html",
-      icon: require('@site/static/img/services/fg.svg').default,
+      icon: FgSvg
     },
     {
       title: "GeminiDB",
       text: "Distributed, Multi-Model NoSQL Database Service",
       link: "https://docs.otc.t-systems.com/geminidb/index.html",
-      icon: require('@site/static/img/services/RDS .svg').default,
+      icon: GeminidbSvg
     },
   ];
 
