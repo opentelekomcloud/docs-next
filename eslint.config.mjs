5c83b8deec2dc06bc5639499f54d0437464a0cba
Sebastian Gode <sebastian.gode@telekom.de>
2024-11-06 10:57:52 +0000

    Fix docusaurus issues

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -5,8 +5,13 @@ import pluginReact from "eslint-plugin-react";
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
-  {files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"]},
-  {languageOptions: { globals: globals.browser }},
+  {
+    files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"],
+    languageOptions: { globals: globals.browser },
+  },
+  {
+    ignores: [".docusaurus/*"],  // Add ignore patterns her
+  },
   ...tseslint.configs.recommended,
-  pluginReact.configs.flat['jsx-runtime'],
+  pluginReact.configs.flat["jsx-runtime"],
 ];
