a50197eb4947e05be1c02ba1cf07ea9154b2e8b8
Kyriakos Akriotis <akriotis.kyriakos@gmail.com>
2024-11-18 15:17:24 +0000

    removed blog, hid caf #116

--- src/components/HomepageFeatures/index.tsx
+++ src/components/HomepageFeatures/index.tsx
@@ -2,10 +2,6 @@ import clsx from 'clsx';
 import Heading from '@theme/Heading';
 import styles from './styles.module.css';
 
-import BestPracticesSvg from '@site/static/img/best-practices.svg';
-import BlueprintsSvg from '@site/static/img/blueprints.svg';
-import CafSvg from '@site/static/img/caf.svg';
-
 type FeatureItem = {
   title: string;
   Svg: React.ComponentType<React.ComponentProps<'svg'>>;
@@ -16,7 +12,7 @@ type FeatureItem = {
 const FeatureList: FeatureItem[] = [
   {
     title: 'Best Practices',
-    Svg: BestPracticesSvg,
+    Svg: require('@site/static/img/best-practices.svg').default,
     description: (
       <>
         Explore the recommended strategies for resource management, such as
@@ -28,7 +24,7 @@ const FeatureList: FeatureItem[] = [
   },
   {
     title: 'Blueprints',
-    Svg: BlueprintsSvg,
+    Svg: require('@site/static/img/blueprints.svg').default,
     description: (
       <>
         Discover tailored out-of-the-box solutions and
@@ -38,34 +34,64 @@ const FeatureList: FeatureItem[] = [
     ),
     link: '/docs/blueprints'
   },
-  {
-    title: 'Cloud Adoption Framework',
-    Svg: CafSvg,
-    description: (
-      <>
-        The Cloud Adoption Framework provides a structured approach for organizations to transition their business to 
-        Open Telekom Cloud. It covers various stages such as strategy, planning, readiness, migration, governance, and management. 
-      </>
-    ),
-    link: '/caf'
-  },
+  // {
+  //   title: 'Cloud Adoption Framework',
+  //   Svg: require('@site/static/img/caf.svg').default,
+  //   description: (
+  //     <>
+  //       The Cloud Adoption Framework provides a structured approach for organizations to transition their business to 
+  //       Open Telekom Cloud. It covers various stages such as strategy, planning, readiness, migration, governance, and management. 
+  //     </>
+  //   ),
+  //   link: '/caf'
+  // },
 ];
+
+// function Feature({title, Svg, description, link}: FeatureItem) {
+//   return (
+//     <div className={clsx('col col--4')}>
+//         <scale-card to={link} class="scale-telekom-feature-card">
+//           <div style={{margin: '-24px'}}>
+//             <div className="text--center">
+//               <Svg className={styles.featureSvg} role="img" />
+//             </div>
+//             <div className="text--center padding-horiz--md" style={{margin: '0 0 16px 0'}}> 
+//               {/* <h4 style={{margin: '0px 0 16px 0', font: 'var(--telekom-text-style-heading-4)'}}>{title} </h4> */}
+//               <Heading as="h3" style={{margin: '8px'}}>{title}</Heading>
+//               <span>
+//                 {description}              
+//               </span>
+//               {/* <scale-button style={{margin: '16px'}} icon-position="after">
+//                 Get Started <scale-icon-navigation-right></scale-icon-navigation-right>
+//               </scale-button>  */}
+//             </div>
+//           </div>
+//         </scale-card>
+//     </div>
+//     );
+// }
+
+// export default function HomepageFeatures(): JSX.Element {
+//   return (
+//     <section className={styles.features}>
+//       <div className="container">
+//         <div className="row">
+//           <div className={clsx('col col--2')}></div>
+//           {FeatureList.map((props, idx) => (
+//             <Feature key={idx} {...props} />
+//           ))}
+//           <div className={clsx('col col--2')}></div>
+//         </div>
+//       </div>
+//     </section>
+//   );
+// }
+
+
 
 function Feature({title, Svg, description, link}: FeatureItem) {
   return (
-    // <div className={clsx('col col--4')}>
-    //   <div className="text--center">
-    //     <Svg className={styles.featureSvg} role="img" />
-    //   </div>
-    //   <div className="text--center padding-horiz--md">
-    //     <Heading as="h3">{title}</Heading>
-    //     <p>{description}</p>
-    //     {/* <scale-button icon-position="after">
-    //       Get Started <scale-icon-navigation-right></scale-icon-navigation-right>
-    //     </scale-button> */}
-    //   </div>
-    // </div>
-    <div className={clsx('col col--4')}>
+    <div className={clsx('col col--5')}>
         <scale-card to={link} class="scale-telekom-feature-card">
           <div style={{margin: '-24px'}}>
             <div className="text--center">
@@ -84,7 +110,6 @@ function Feature({title, Svg, description, link}: FeatureItem) {
           </div>
         </scale-card>
     </div>
-
     );
 }
 
@@ -93,9 +118,11 @@ export default function HomepageFeatures(): JSX.Element {
     <section className={styles.features}>
       <div className="container">
         <div className="row">
+          <div className={clsx('col col--1')}></div>
           {FeatureList.map((props, idx) => (
             <Feature key={idx} {...props} />
           ))}
+          <div className={clsx('col col--1')}></div>
         </div>
       </div>
     </section>
