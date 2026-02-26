78834124ed69e4aa20f8cc4176311b2edcccbb9e
Kyriakos Akriotis <akriotis.kyriakos@gmail.com>
2026-02-26 12:32:12 +0000

    fix take 3 the 'empty UMAMI_*' bug for forks  #353 #368

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -328,7 +328,7 @@ const config: Config = {
       {
         websiteID: process.env.UMAMI_WEBSITE_ID?.trim() || "00000000-0000-0000-0000-000000000000", // Required
         analyticsDomain: process.env.UMAMI_ANALYTICS_DOMAIN?.trim() || "opentelekomcloud.github.io", // Required
-        dataHostURL: process.env.UMAMI_DATAHOST_URL, // Optional
+        dataHostURL: process.env.UMAMI_DATAHOST_URL?.trim() ?? "opentelekomcloud-analytics.github.io", // Optional
         dataAutoTrack: true, // Optional
         dataDoNotTrack: true, // Optional
         dataCache: true, // Optional
