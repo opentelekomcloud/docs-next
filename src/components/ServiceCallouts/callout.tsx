f107ef0d59c47d69fe47e62925c4180d9a8a1ad2
Kyriakos Akriotis <akriotis.kyriakos@gmail.com>
2026-03-03 09:30:29 +0000

    fixed proportions for service svgs in cards #370

--- src/components/ServiceCallouts/callout.tsx
+++ src/components/ServiceCallouts/callout.tsx
@@ -15,19 +15,30 @@ export interface Callout {
 }
 function Callout(props: Callout): JSX.Element {
   return (
-<div className={clsx(styles.odscard__feature_content)}>
+    <div className={clsx(styles.odscard__feature_content)}>
       <ODSCardFeature
         contentSlot={
-            <ODSCardFeaturePreferredContent
-              productName={props.title}
-              productPrice={props.text ?? ""}
-            />
+          <ODSCardFeaturePreferredContent
+            productName={props.title}
+            productPrice={props.text ?? ""}
+          />
         }
         imageProps={{ src: useBaseUrl(props.icon), alt: props.title }}
-        buttonProps={{ onClick: () => {} }}
+        buttonProps={{
+          onClick: () => {
+            const currentDomain = window.location.hostname;
+            const linkDomain = new URL(props.link, window.location.origin).hostname;
+
+            if (currentDomain === linkDomain) {
+              window.location.href = props.link;
+            } else {
+              window.open(props.link, '_blank');
+            }
+          }
+        }}
       />
     </div>
-);
+  );
 }
 
 export interface ServiceCalloutsProps {
@@ -36,7 +47,7 @@ export interface ServiceCalloutsProps {
 
 export default function ServiceCallouts({ callouts }: ServiceCalloutsProps): JSX.Element {
   return (
-    <section className="container" style={{marginTop: "30px"}}>
+    <section className="container" style={{ marginTop: "30px" }}>
       <div className="row">
         {callouts.map((c) => (
           <div key={c.title} className="col col--6">
