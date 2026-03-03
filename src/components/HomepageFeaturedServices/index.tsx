5b213bb55350a55b2e076be7829e1144f17a7662
Kyriakos Akriotis <akriotis.kyriakos@gmail.com>
2026-03-03 00:53:38 +0000

    fixed new features btns #370

--- src/components/HomepageFeaturedServices/index.tsx
+++ src/components/HomepageFeaturedServices/index.tsx
@@ -3,6 +3,7 @@ import styles from './styles.module.css';
 import FeaturedServices from '../ServiceCallouts/FeaturedServices';
 import NewServices from '../ServiceCallouts/NewServices';
 import useBaseUrl from '@docusaurus/useBaseUrl';
+import { ODSButton } from '@telekom-ods/react-ui-kit';
 
 export default function HomepageFeaturedServices(): JSX.Element {
   return (
@@ -14,15 +15,26 @@ export default function HomepageFeaturedServices(): JSX.Element {
               Explore our Featured Services
             </h1>
             <p className="margin-bottom--md">
-              Check out our collection of technical solutions, best practices and cool tips for the most popular services of Open Telekom Cloud.
+              Check out our collection of technical solutions, best practices and cool tips for the most popular services of T Cloud Public.
             </p>
-            <a className="button button--secondary" href={useBaseUrl("/portfolio")}>
+
+            {/* <a className="button button--secondary" href={useBaseUrl("/portfolio")}>
               Open Portfolio Navigator
-            </a>
+            </a> */}
           </div>
         </div>
+
         <div className="col col--8" aria-hidden="true" style={{ paddingTop: '40px' }}>
           <FeaturedServices />
+        </div>
+        <div className={clsx("col col--12", styles.btn__inner)}>
+          <ODSButton
+                variant="primary"
+                label="Open Portfolio Navigator"
+                size="small"
+                href={useBaseUrl("/portfolio")}
+                target="_self"
+              />
         </div>
         {/* <scale-divider></scale-divider> */}
         <div className={clsx("col col--4", styles.item__inner)}>
@@ -33,15 +45,23 @@ export default function HomepageFeaturedServices(): JSX.Element {
             <p className="margin-bottom--md">
               Explore the technical documentation of our brand new services in Help Center.
             </p>
-            <a className="button button--secondary" href="https://www.open-telekom-cloud.com/en/products-services/roadmap" target="_blank">
+            {/* <a className="button button--secondary" href="https://www.open-telekom-cloud.com/en/products-services/roadmap" target="_blank">
               Check the Portfolio Roadmap
-            </a>
+            </a> */}
           </div>
         </div>
         <div className="col col--8" aria-hidden="true" style={{ paddingTop: '40px' }}>
           <NewServices />
         </div>
-
+        <div className={clsx("col col--12", styles.btn2__inner)}>
+          <ODSButton
+                variant="secondary"
+                label="Check the Portfolio Roadmap"
+                size="small"
+                href="https://www.open-telekom-cloud.com/en/products-services/roadmap"
+                target="_blank"
+              />
+        </div>
       </div>
     </section >
   );
