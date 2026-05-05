230119c99fb9a039bd472ae83cab327f54b89a44
Kyriakos Akriotis <akriotis.kyriakos@gmail.com>
2026-05-05 12:54:28 +0000

    added brand text #369

--- src/theme/Navbar/Content/index.tsx
+++ src/theme/Navbar/Content/index.tsx
@@ -1,11 +1,11 @@
-import {type ReactNode} from 'react';
+import { type ReactNode } from 'react';
 import clsx from 'clsx';
-import {useThemeConfig, ErrorCauseBoundary} from '@docusaurus/theme-common';
+import { useThemeConfig, ErrorCauseBoundary } from '@docusaurus/theme-common';
 import {
   splitNavbarItems,
   useNavbarMobileSidebar,
 } from '@docusaurus/theme-common/internal';
-import NavbarItem, {type Props as NavbarItemConfig} from '@theme/NavbarItem';
+import NavbarItem, { type Props as NavbarItemConfig } from '@theme/NavbarItem';
 import NavbarColorModeToggle from '@theme/Navbar/ColorModeToggle';
 import SearchBar from '@theme/SearchBar';
 import NavbarMobileSidebarToggle from '@theme/Navbar/MobileSidebar/Toggle';
@@ -19,7 +19,7 @@ function useNavbarItems() {
   return useThemeConfig().navbar.items as NavbarItemConfig[];
 }
 
-function NavbarItems({items}: {items: NavbarItemConfig[]}): JSX.Element {
+function NavbarItems({ items }: { items: NavbarItemConfig[] }): JSX.Element {
   return (
     <>
       {items.map((item, i) => (
@@ -30,7 +30,7 @@ function NavbarItems({items}: {items: NavbarItemConfig[]}): JSX.Element {
               `A theme navbar item failed to render.
 Please double-check the following navbar item (themeConfig.navbar.items) of your Docusaurus config:
 ${JSON.stringify(item, null, 2)}`,
-              {cause: error},
+              { cause: error },
             )
           }>
           <NavbarItem {...item} />
@@ -68,24 +68,30 @@ export default function NavbarContent(): JSX.Element {
 
   return (
     <NavbarContentLayout
-  left={
-    <>
-      {!mobileSidebar.disabled && <NavbarMobileSidebarToggle />}
-      <NavbarLogo />
-    </>
-  }
-  center={<NavbarItems items={leftItems} />}
-  right={
-    <>
-      <NavbarItems items={rightItems} />
-      <NavbarColorModeToggle className={styles.colorModeToggle} />
-      {!searchBarItem && (
-        <NavbarSearch>
-          <SearchBar />
-        </NavbarSearch>
-      )}
-    </>
-  }
-/>
+      left={
+        <>
+          {!mobileSidebar.disabled && <NavbarMobileSidebarToggle />}
+          <div className={styles.brandBlock}>
+            <NavbarLogo />
+            <div className={styles.brandText}>
+              <span>T Cloud Public</span>
+              <span>Architecture Center</span>
+            </div>
+          </div>
+        </>
+      }
+      center={<NavbarItems items={leftItems} />}
+      right={
+        <>
+          <NavbarItems items={rightItems} />
+          <NavbarColorModeToggle className={styles.colorModeToggle} />
+          {!searchBarItem && (
+            <NavbarSearch>
+              <SearchBar />
+            </NavbarSearch>
+          )}
+        </>
+      }
+    />
   );
 }
