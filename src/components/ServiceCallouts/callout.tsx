3a1e7d90b7d1276169bf8c25f9470f7f508c87e5
Kyriakos Akriotis <akriotis.kyriakos@gmail.com>
2026-05-06 09:41:40 +0000

    changes callout stripes #369

--- src/components/ServiceCallouts/callout.tsx
+++ src/components/ServiceCallouts/callout.tsx
@@ -1,35 +1,38 @@
-import clsx from "clsx";
-import styles from "./styles.module.css";
+import clsx from 'clsx';
+import styles from './styles.module.css';
 import { ComponentType, SVGProps } from 'react';
 import useBaseUrl from '@docusaurus/useBaseUrl';
-
 
 export interface Callout {
   title: string;
   text?: string;
   link: string;
-
-  // icon: (props: React.ComponentProps<"svg">) => JSX.Element;
   icon: ComponentType<SVGProps<SVGSVGElement>>;
 }
+
 function Callout(props: Callout): JSX.Element {
   return (
-    <a href={useBaseUrl(props.link)} className={styles.callout__link}>
-      <div className={clsx("card", styles.callout, styles.callout__shadow)} style={{marginBottom: '30px'}}>
-        <div className="card__header">
-          <h3 style={{ fontWeight: 'bold', fontSize: 'medium', alignItems: 'center', display: 'flex', marginBottom: '0'}}>
-            <props.icon className={styles.callout__icon} stroke="currentColor" aria-hidden="true" role="img"/>
+    <a href={useBaseUrl(props.link)} className={styles.calloutLink}>
+      <article className={styles.callout}>
+        <div className={styles.calloutHeader}>
+          <props.icon
+            className={styles.calloutIcon}
+            stroke="currentColor"
+            aria-hidden="true"
+            role="img"
+          />
+
+          <h3 className={styles.calloutTitle}>
             {props.title}
           </h3>
         </div>
-        <div className="card__body" style={{paddingTop: '0'}}>
-        <span className="link--inset" aria-hidden="true"></span>
-          <p>
+
+        {props.text && (
+          <p className={styles.calloutText}>
             {props.text}
           </p>
-        </div>
-        <div className={styles.callout__bottom} />
-      </div>
+        )}
+      </article>
     </a>
   );
 }
@@ -40,7 +43,7 @@ export interface ServiceCalloutsProps {
 
 export default function ServiceCallouts({ callouts }: ServiceCalloutsProps): JSX.Element {
   return (
-    <section className="container">
+    <section className={styles.calloutsSection}>
       <div className="row">
         {callouts.map((c) => (
           <div key={c.title} className="col col--4">
