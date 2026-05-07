dae1846597d6f9039f2d5825680058aa4bb7c227
Kyriakos Akriotis <akriotis.kyriakos@gmail.com>
2026-05-07 06:38:51 +0000

    added revamped tags page #369

---
+++ src/theme/Tag/index.tsx
@@ -0,0 +1,27 @@
+import React, {type ReactNode} from 'react';
+import clsx from 'clsx';
+import Link from '@docusaurus/Link';
+import type {Props} from '@theme/Tag';
+
+import styles from './styles.module.css';
+
+export default function Tag({
+  permalink,
+  label,
+  count,
+  description,
+}: Props): ReactNode {
+  return (
+    <Link
+      rel="tag"
+      href={permalink}
+      title={description}
+      className={clsx(
+        styles.tag,
+        count ? styles.tagWithCount : styles.tagRegular,
+      )}>
+      {label}
+      {count && <span>{count}</span>}
+    </Link>
+  );
+}
