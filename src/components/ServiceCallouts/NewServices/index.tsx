ed8aa249fcfd10800f61f4c96060b3483bea6f61
Kyriakos Akriotis <akriotis.kyriakos@gmail.com>
2026-08-17 09:07:22 +0000

    updated featured, new services and featured articles #439

--- src/components/ServiceCallouts/NewServices/index.tsx
+++ src/components/ServiceCallouts/NewServices/index.tsx
@@ -1,67 +1,63 @@
 import ServiceCallouts, { Callout } from '../callout';
 
-import AsmSvg from '@site/static/img/services/asm.svg'
 import CciSvg from '@site/static/img/services/cci.svg'
-import OcrSvg from '@site/static/img/services/ocr.svg'
-import FgSvg from '@site/static/img/services/fg.svg'
-import RfsSvg from '@site/static/img/services/rfs.svg'
 import CfwSvg from '@site/static/img/services/cfw.svg'
+import DmsRMQSvg from '@site/static/img/services/dms_rocketmq.svg'
+import UcsSvg from '@site/static/img/services/ucs.svg'
+import CcnSvg from '@site/static/img/services/ccn.svg'
+import EswSvg from '@site/static/img/services/esw.svg'
+import ErSvg from '@site/static/img/services/er.svg'
 
- const calloutsList: Callout[] = [
-    {
-      title: "ASM",
-      text: "Application Service Mesh (Istio-Based)",
-      link: "https://docs.otc.t-systems.com/application-service-mesh/index.html",
-      icon: AsmSvg
-    },
-    {
-      title: "CCI",
-      text: "Serverless Container Engine",
-      link: "https://docs.otc.t-systems.com/cloud-container-instance/index.html",
-      icon: CciSvg
-    },
-    {
-      title: "RFS",
-      text: "Resource Formation Service",
-      link: "https://docs.otc.t-systems.com/resource-formation-service/index.html",
-      icon: RfsSvg
-    },
-    // {
-    //   title: "Config",
-    //   text: "Continuously Evaluate Resource Configuration",
-    //   link: "https://docs.otc.t-systems.com/config/index.html",
-    //   icon: ConfigSvg
-    // },
-    {
-      title: "OCR",
-      text: "Optical Character Recognition",
-      link: "https://docs.otc.t-systems.com/optical-character-recognition/index.html",
-      icon: OcrSvg
-    },
-    {
-      title: "FGS",
-      text: "FunctionGraph",
-      link: "https://docs.otc.t-systems.com/function-graph/index.html",
-      icon: FgSvg
-    },
-    {
-      title: "CFW",
-      text: "Cloud Firewall",
-      link: "https://docs.otc.t-systems.com/cloud-firewall/index.html",
-      icon: CfwSvg
-    },
-    // {
-    //   title: "GeminiDB",
-    //   text: "Distributed, Multi-Model NoSQL Database Service",
-    //   link: "https://docs.otc.t-systems.com/geminidb/index.html",
-    //   icon: GeminidbSvg
-    // },
-  ];
+const calloutsList: Callout[] = [
+  {
+    title: "UCS",
+    text: "Ubiquitous Cloud Native Service",
+    link: "https://docs.otc.t-systems.com/ubiquitous-cloud-native-service/index.html",
+    icon: UcsSvg
+  },
+  {
+    title: "DMS (for RocketMQ)",
+    text: "Distributed Message Service",
+    link: "https://docs.otc.t-systems.com/distributed-message-service-rocketmq/index.html",
+    icon: DmsRMQSvg
+  },
+  {
+    title: "CCI",
+    text: "Serverless Container Engine",
+    link: "https://docs.otc.t-systems.com/cloud-container-instance/index.html",
+    icon: CciSvg
+  },
+  {
+    title: "CCN",
+    text: "Cloud Connect",
+    link: "https://docs.otc.t-systems.com/cloud-connect/index.html",
+    icon: CcnSvg
+  },
+   {
+    title: "ER",
+    text: "Enterprise Router",
+    link: "https://docs.otc.t-systems.com/enterprise-router/index.html",
+    icon: ErSvg
+  },
+  {
+    title: "ESW",
+    text: "Enterprise Switch",
+    link: "https://docs.otc.t-systems.com/enterprise-switch/index.html",
+    icon: EswSvg
+  },
+ 
+  // {
+  //   title: "GeminiDB",
+  //   text: "Distributed, Multi-Model NoSQL Database Service",
+  //   link: "https://docs.otc.t-systems.com/geminidb/index.html",
+  //   icon: GeminidbSvg
+  // },
+];
 
 export default function NewServices(): JSX.Element {
-    return (
-        <div>
-            <ServiceCallouts callouts={calloutsList} />
-        </div>
-    );
+  return (
+    <div>
+      <ServiceCallouts callouts={calloutsList} />
+    </div>
+  );
 }
